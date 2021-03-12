a07edb92e52a7a30b28da56d50ba95cd029dfb17
MaheKarim <mahekarim@gmail.com>
2021-03-12 17:35:19 +0600

    ⬆ Fuel Seeder️

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -42,11 +42,7 @@ class LoginController extends Controller
 
    }
 
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
+
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
---
+++ database/seeds/FuelTypeSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class FuelTypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+
+        public function run()
+    {
+        App\FuelType::create([
+            'id' => '1',
+            'fuel_name' => 'Diseal',
+
+        ]);
+
+        App\FuelType::create([
+            'id' => '2',
+            'fuel_name' => 'Petrol'
+        ]);
+    }
+
+}
